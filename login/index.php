ae3631c8519c55513d657d88931b136ea2429d0a
Kyano <104148@glr.nl>
2026-06-11 10:13:02 +0200

    index

--- login/index.php
+++ login/index.php
@@ -1,1 +1,15 @@
 <?php
+require 'dbconfig.php';
+
+$query = "SELECT * FROM login";
+$stmt = $conn->prepare($query);
+$stmt->execute();
+
+//Haal alle resultaten uit de array op
+$resultaten = $stmt->fetchAll();
+
+//Aantal resultaten tellen
+$aantalRijen = count($resultaten);
+
+
+include 'index_view.php';
