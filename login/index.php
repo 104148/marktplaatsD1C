a4742f56afe464c940aa35ae0fe47d5eeab3f89c
104148 <104148@glr.nl>
2026-06-12 16:05:49 +0200

    up to date met main branch

--- login/index.php
+++ login/index.php
@@ -11,5 +11,47 @@ $resultaten = $stmt->fetchAll();
 //Aantal resultaten tellen
 $aantalRijen = count($resultaten);
 
+$login = '
+<title>Login</title>
+<form action="login.php" method="post">
+    <h1>Login</h1>
+<label for="user">Gebruikersnaam</label><br>
+<input type="text" id="user" name="user"><br><br>
+
+
+    <label for="password">Wachtwoord:</label><br>
+    <input type="password" id="password" name="password"><br><br>
+
+    <input type="submit" value="Verzenden">
+</form>';
+//----------------------------------------------------------------------------------
+$register = '
+<title>Registreer</title>
+<form action="register.php" method="post">
+    <h1>Registreer</h1>
+    <label for="user1">Gebruikersnaam</label><br>
+    <input type="text" id="user1" name="user1"><br><br>
+
+
+    <label for="password1">Wachtwoord:</label><br>
+    <input type="password" id="password1" name="password1"><br><br>
+
+    <input type="submit" value="Verzenden">
+</form>';
+
+if (isset($_POST["wissel_knop"]) && $_POST["wissel_knop"] === "Registreer") {
+    // Als er op 'Registreer' werd geklikt, tonen we het registreerformulier
+    // en zetten we de knoptekst voor de volgende keer op 'Login'
+    $show = $register;
+    $knoptxt = "Login";
+} else {
+    // STANDAARD (eerste keer laden) en als er op 'Login' werd geklikt:
+    // we tonen het loginformulier en zetten de knoptekst op 'Registreer'
+    $show = $login;
+    $knoptxt = "Registreer";
+}
+
 
 include 'index_view.php';
+
+//test
--- login/index_view.php
+++ login/index_view.php
@@ -2,35 +2,17 @@
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 </head>
 <body>
 
-<form action="login.php" method="post">
-    <h1>Login</h1>
-<label for="user">Gebruikersnaam</label><br>
-<input type="text" id="user" name="user"><br><br>
+<?= $show ?>
 
+<hr>
 
-    <label for="password">Wachtwoord:</label><br>
-    <input type="password" id="password" name="password"><br><br>
-
-    <input type="submit" value="Verzenden">
-</form>
-
-<form action="register.php" method="post">
-    <h1>Registreer</h1>
-    <label for="user1">Gebruikersnaam</label><br>
-    <input type="text" id="user1" name="user1"><br><br>
-
-
-    <label for="password1">Wachtwoord:</label><br>
-    <input type="password" id="password1" name="password1"><br><br>
-
-    <input type="submit" value="Verzenden">
+<form action="" method="post">
+    <input type="submit" name="wissel_knop" value="<?= $knoptxt ?>">
 </form>
 
 </body>
