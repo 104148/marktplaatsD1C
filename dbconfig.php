4031935dfcdeb732436a8bc766185c65f7b39ccf
Kyano <104148@glr.nl>
2026-06-16 11:00:01 +0200

    config

---
+++ dbconfig.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$db = '../db.sqlite';
+try {
+    $conn = new PDO("sqlite:$db");
+//    echo 'Connected to database! <br><br>';
+    echo "<script>console.log('Connected to database!');</script>";
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+//Foutmeldingen zichtbaar maken
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
+
+$loginbtn = "";
+
+if ($_SESSION["ingelogged"] === "true") {
+    $loginbtn = '<?= $_SESSION["user" ?>';
+} else {
+$loginbtn = '                            <div class="col-md-3"><a class="dropdown-item text-center rounded bg-light py-2" href="./login">Login</a></div>
+                            <div class="col-md-3"><a class="dropdown-item text-center rounded bg-light py-2" href="#">Register</a></div>';
+}
 include_once("index_view.php");
 
 ?>
